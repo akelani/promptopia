00d0d05064aeef05e53b57e632432a49542092ba
Anthony Kelani <akelani@gmail.com>
2023-05-20 00:53:15 +0000

    Profile functionality complete.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,10 @@ import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState('');
 
   const handleCopy = () => {
@@ -35,7 +39,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           </div>
         </div>
 
-        <div className="copy_btn" onClick={() => {handleCopy}}>
+        <div className="copy_btn" onClick={() => { handleCopy }}>
           <Image
             src={copied === post.prompt
               ? '/assets/icons/tick.svg'
@@ -50,8 +54,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <p className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}>
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}>
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
